4d7ba16b3277b73e6ac9dbf721de046cdeb12884
Petr Bogdansky <441066@gmail.com>
2020-11-15 03:00:12 +0300

    Add task 3

---
+++ task3.js
@@ -0,0 +1,9 @@
+let chess = "";
+let size = 10;
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    (j + i) % 2 == 0 ? (chess += " ") : (chess += "#");
+  }
+  chess += "\n";
+}
+console.log(chess);
