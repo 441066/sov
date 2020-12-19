def21102202cbf67ad9df12b90bee2936446cc9e
Petr Bogdansky <441066@gmail.com>
2020-12-20 02:47:57 +0300

    Add JSDoc to task-g-1.js

--- task-g-1.js
+++ task-g-1.js
@@ -1,12 +1,20 @@
-/*
-fill([1, 2, 3], 'x');             // ['x', 'x', 'x']
-fill([2, 4, 6, 8], '*', 1, 3);    // [2, '*', '*', 8]
-fill([1, 3, 5, 7], '#', -2, 2);   // ['#', '#', 5, 7]
-fill([1, 3, 5, 7], '#', 2, 10);   // [1, 3, '#', '#']
-*/
-
-let arrayOfJakob = [4, 8, 15, 16, 23, 42];
-
+/**
+ * @namespace SovGoal
+ */
+/**
+ * Заменяет элементы массива переданными символами.
+ * @memberof SovGoal
+ * @param {Array} arrayToFill - массив, который будем обрабатывать.
+ * @param {String} symbol - символ (или строка) на которую будем заменять элементы массива.
+ * @param {Number} startIndex - начальный индекс элемента массива, который будем заменять.
+ * @param {Number} endIndex - конечный индекс элемента массива, который будем заменять.
+ * @returns {Array} - итоговый массив, в котором заменены элементы.
+ * @example
+ * fill([1, 2, 3], 'x');
+ * // -> ['x', 'x', 'x']
+ * fill([2, 4, 6, 8], '*', 1, 3);
+ * // -> [2, '*', '*', 8]
+ */
 let fill = (arrayToFill, symbol, startIndex = 0, endIndex = 0) => {
     if (endIndex == 0) {
         endIndex = arrayToFill.length;
@@ -18,6 +26,8 @@ let fill = (arrayToFill, symbol, startIndex = 0, endIndex = 0) => {
     }
     return arrayToFill;
 };
+
+let arrayOfJakob = [4, 8, 15, 16, 23, 42];
 
 console.log(fill(arrayOfJakob, "*", 2, 4));
 
